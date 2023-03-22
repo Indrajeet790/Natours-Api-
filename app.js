d01cc5fea58e2c1201a768ea69132c94a3dc244e
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-03-22 10:59:04 +0530

    create routes for  READ all tours

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require("express");
-
+const fs = require("fs");
 const app = express();
 const port = 8000;
+
+// Converts a JavaScript Object Notation (JSON) string into an object.
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+// create tours route(get)
+app.get("/api/v1/tours", (req, resp) => {
+  resp.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
 
 app.listen(port, () => {
   console.log(`app is running on port ${port}`);
